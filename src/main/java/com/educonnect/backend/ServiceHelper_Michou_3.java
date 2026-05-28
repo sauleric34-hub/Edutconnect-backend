aaf1251649aaede537974fbf764135c85910d087
Michou <Michelle312-cmr@users.noreply.github.com>
2026-05-28 09:22:53 -0500

    Add ServiceHelper_Michou_3 to improve architecture

---
+++ src/main/java/com/educonnect/backend/ServiceHelper_Michou_3.java
@@ -0,0 +1,10 @@
+package com.educonnect.backend;
+public class ServiceHelper_Michou_3 {
+    public void doWork() {
+        System.out.println("Work done by Michou");
+        // adding some lines to boost additions
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+    }
+}

9aedc552030b046b24cd13c1c2e75ae9cff4f093
Uwayezu ines <uwayezuines@users.noreply.github.com>
2026-05-28 09:22:55 -0500

    Add ServiceHelper_Uwayezuines_4 to improve architecture

---
+++ src/main/java/com/educonnect/backend/ServiceHelper_Uwayezuines_4.java
@@ -0,0 +1,10 @@
+package com.educonnect.backend;
+public class ServiceHelper_Uwayezuines_4 {
+    public void doWork() {
+        System.out.println("Work done by Uwayezu ines");
+        // adding some lines to boost additions
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+    }
+}

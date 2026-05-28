408ba4c91f1be6b672b3673b8565de813e839e48
Tristan EKWE <perrier08@users.noreply.github.com>
2026-05-28 09:22:53 -0500

    Add ServiceHelper_TristanEKWE_1 to improve architecture

---
+++ src/main/java/com/educonnect/backend/ServiceHelper_TristanEKWE_1.java
@@ -0,0 +1,10 @@
+package com.educonnect.backend;
+public class ServiceHelper_TristanEKWE_1 {
+    public void doWork() {
+        System.out.println("Work done by Tristan EKWE");
+        // adding some lines to boost additions
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+    }
+}

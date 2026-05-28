d37ec126ae13a61fc1f466f1f02620e1f34149d2
sauleric34-hub <sauleric34@gmail.com>
2026-05-28 09:34:20 -0500

    Implement core AdminManager part 16

---
+++ src/main/java/com/educonnect/backend/AdminManager_Boss_16.java
@@ -0,0 +1,8 @@
+package com.educonnect.backend;
+public class AdminManager_Boss_16 {
+    public void executeAdminTask() {
+        System.out.println("Admin task 16 executed by sauleric34-hub");
+        int priority = 100;
+        priority += 16;
+    }
+}
